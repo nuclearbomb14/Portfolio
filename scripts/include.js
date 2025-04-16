2d80995e176bed04033243b10ec1038c0d41cd06
nuclearbomb14 <jimmyk7002@gmail.com>
2025-04-16 13:56:05 -0400

    Change

--- scripts/include.js
+++ scripts/include.js
@@ -1,9 +1,23 @@
-window.onload = function() {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-container').innerHTML = data;
+window.onload = function () {
+    const loadCSS = href => {
+        return new Promise(resolve => {
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = href;
+            link.onload = resolve;
+            document.head.appendChild(link);
         });
+    };
+
+    Promise.all([
+        loadCSS('../style/universal.css'),
+        loadCSS('../style/header.css')
+    ]).then(() => {
+        return fetch('header.html');
+    }).then(response => response.text())
+      .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+    });
 
     fetch('footer.html')
         .then(response => response.text())
